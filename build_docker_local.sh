9269b92c3c85e868697c2e368ede9b91fdab73ba
yjkim93 <youngjunkim@gmx.com>
2025-12-07 22:27:02 +0000

    only downstream module

--- build_docker_local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-tag=$(cat cellbender/__init__.py | sed -e 's?__version__ = ??' | sed "s/^'\(.*\)'$/\1/")
-
-docker build \
-    -t us.gcr.io/broad-dsde-methods/cellbender:${tag} \
-    -f docker/Dockerfile \
-    .
